0804b5f6fbb47c49c1dc143f65b7de35c5cb3997
Matheca123 <mateuscorreia14@gmail.com>
2025-12-18 02:14:30 -0300

    Upd_TelaRecuperacaoSenha

--- App-React-Native-KinCoin/src/pages/style/StyleRecuperacaoSenha.ts
+++ App-React-Native-KinCoin/src/pages/style/StyleRecuperacaoSenha.ts
@@ -3,13 +3,13 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0A1F3D",
-    paddingHorizontal: 20,
-    paddingTop: 80,
+    backgroundColor: "#001B2E",
+    padding: 20,
+    justifyContent: "center",
   },
 
   title: {
-    color: "white",
+    color: "#fff",
     fontSize: 22,
     fontWeight: "bold",
     textAlign: "center",
@@ -17,43 +17,49 @@ const styles = StyleSheet.create({
   },
 
   description: {
-    color: "white",
-    fontSize: 15,
+    color: "#fff",
+    fontSize: 16,
     textAlign: "center",
-    marginBottom: 40,
+    marginBottom: 20,
     lineHeight: 22,
     paddingHorizontal: 10,
   },
 
   input: {
     width: "100%",
-    height: 50,
-    backgroundColor: "white",
-    borderRadius: 8,
-    paddingHorizontal: 15,
-    marginBottom: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 12,
+    marginBottom: 5,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+
+  erroTexto: {
+    color: "red",
+    marginBottom: 10,
+    marginLeft: 5,
   },
 
   button: {
     width: "100%",
-    height: 50,
-    backgroundColor: "#FF8C00",
-    borderRadius: 8,
+    backgroundColor: "#F7931A",
+    paddingVertical: 15,
+    borderRadius: 10,
     alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 25,
+    marginBottom: 15,
   },
 
   buttonText: {
-    color: "white",
-    fontSize: 16,
+    color: "#fff",
     fontWeight: "bold",
+    fontSize: 16,
   },
 
   cancel: {
-    color: "white",
+    color: "#F7931A",
     textAlign: "center",
-    textDecorationLine: "underline",
+    marginTop: 10,
     fontSize: 14,
   },
 });
