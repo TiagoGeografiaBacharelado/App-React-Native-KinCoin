5cab83ea752bbb06c52daa640767236d5ce1c2ad
Matheca123 <mateuscorreia14@gmail.com>
2025-12-18 01:38:09 -0300

    Upd_Experiencia_Tela_Conclusao

--- App-React-Native-KinCoin/src/pages/TelaConclusao.jsx
+++ App-React-Native-KinCoin/src/pages/TelaConclusao.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
+import { useRoute } from "@react-navigation/native"; // Para ler params
 import style from "./style/StyleTelaConclusao";
 import Esquilo from "../assets/images/EsquiloComemorando.png";
+import useExperiencia from "./script/ScriptUseExperiencia";
 
 export default function TelaConclusao({ navigation }) {
+  const { ganharXP } = useExperiencia();
+  const route = useRoute();
+  const xpGanho = route.params?.xpGanho || 25; // Padrão 25 se não passado
+
+  useEffect(() => {
+    // Concede o XP calculado
+    ganharXP(xpGanho);
+  }, []);
+
   async function continuar() {
     navigation.reset({
       index: 0,
@@ -17,7 +28,7 @@ export default function TelaConclusao({ navigation }) {
 
       <Text style={style.titulo}>Você por acaso é um gênio?</Text>
 
-      <Text style={style.xp}>⭐ Você recebeu +25 de experiência</Text>
+      <Text style={style.xp}>⭐ Você recebeu +{xpGanho} de experiência</Text>
 
       <TouchableOpacity style={style.botao} onPress={continuar}>
         <Text style={style.botaoTexto}>Continuar</Text>
